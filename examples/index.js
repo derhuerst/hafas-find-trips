356b9d46a3bf3a2b931ea9e7cde140b53d269ef4
Jannis R <mail@jannisr.de>
2018-07-28 04:14:15 +0200

    match based on shape similarity: minor tweaks

--- examples/index.js
+++ examples/index.js
@@ -6,17 +6,23 @@ const findTrips = require('..')
 // const recording = require('./stadtbahn.json')
 const recording = require('./u2-gleisdreieck.json')
 // const recording = require('./u2-stadtmitte.json')
+// const recording = require('./u2-alexanderplatz.json')
+// const recording = require('./u5-alexanderplatz.json')
+// const recording = require('./u3-wittenbergplatz.json')
 
 const hafas = createHafas('hafas-find-trips-example')
 
 findTrips(hafas, {recording})
 .then((matches) => {
-	for (let match of matches) {
+	matches
+	.filter(m => m.score < 250)
+	.sort((a, b) => a.score - b.score)
+	.forEach((match) => {
 		const m = match.movement
 		const n = m.line && m.line.name
 		console.error(n, m.direction, match.score)
 		// console.error(JSON.stringify(match.track))
-	}
+	})
 })
 .catch((err) => {
 	console.error(err)
